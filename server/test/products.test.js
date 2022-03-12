308aa6937188d1c655985ea79c0dac06824a03a0
Alvina Kwon <minjina90@gmail.com>
2022-03-12 15:51:05 -0600

    added test specific description.

--- server/test/products.test.js
+++ server/test/products.test.js
@@ -3,12 +3,12 @@ const supertest = require('supertest');
 const request = supertest(app);
 
 describe('products', () => {
-  it('should return...', async () => {
+  it('should return 200 status code upon successful get request for product 1.', async () => {
     const response = await request.get('/products/1')
     expect(response.status).toBe(200);
   });
 
-  it('should return...', async () => {
+  it('should return 200 status code upon successful get request for all styles of product 1.', async () => {
     const response = await request.get('/products/1/styles')
     expect(response.status).toBe(200);
   });
